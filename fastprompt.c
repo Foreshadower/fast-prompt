ee913e637e0c4d2eb51bdfb91259ae6bf7edf3a8
Foreshadower <jgeatermilton@gmail.com>
2025-07-06 03:14:32 +0100

    minor change pre debug for Arch term

--- fastprompt.c
+++ fastprompt.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
-#include <windows.h>
 #include <string.h>
 #include <stdlib.h>
+
+
+// TODO set color to white at end so terminal doesnt act up
+
 
 const char* fast_basename(int* length) {
     char* path = getenv("PWD");
@@ -13,24 +16,17 @@ const char* fast_basename(int* length) {
     return start;
 }
 
-int main(int argc, char** argv) { // TODO likely much better method
-    // SetConsoleOutputCP(CP_UTF8);
-    // SetConsoleCP(CP_UTF8);
-    char BufTotal[30] = "\x1b[32m"; // TODO Boldly assumes basename is <18 chars
+int main(int argc, char** argv) {
+    char BufTotal[30] = "\x1b[90m"; // TODO Boldly assumes basename is <18 chars
     int len_dir;
     const char* dir = fast_basename(&len_dir);
     memcpy(BufTotal+5, dir, len_dir);
     int index = 5 + len_dir;
 
-    // const char* status_str = argv[1];
-
     char* bufindex = BufTotal+index;
     if (argv[1] && argv[1][0] != '0') {
-        // char buf[10] = {'\x1b', '[', '3', '1', 'm', ' ', ' ', ' ', ' ', ' '};
         char buf[10] = {'\x1b', '[', '3', '1', 'm', ' ', ' ', ' ', argv[1][0], ' '};
-        // int len = strlen(status_str);
-        // instead just check indexes 1 2 and 3 directly, only place for em!
-		if (argv[1][2] == '\0') { // second
+        if (argv[1][2] == '\0') { // second
 			buf[8] = argv[1][1];
 			buf[7] = argv[1][0];
 		} else if (argv[1][3] == '\0') { // third

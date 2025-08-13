23bcaa9585e45a74377f59d2bb31c79ab9409dfc
Foreshadower <jgeatermilton@gmail.com>
2025-08-13 14:59:32 +0100

    readability changes + variables global now

--- fastprompt.c
+++ fastprompt.c
@@ -2,43 +2,57 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* note: the unrolled char assignment annoys me too. */
 
-const char* fast_basename(int* length) {
+int len_dir;
+const char* dir;
+char BufTotal[70] = "\x1b[32m"; // TODO max basename check
+
+const char* basename() {
     char* path = getenv("PWD");
     const char* end = path + strlen(path) - 1;
     const char* start = end;
-    // const char* start = path + strlen(path) - 1;
     while (start > path && start[-1] != '/') start--;
-    *length = (end - start) + 1;
+    len_dir = (end - start) + 1;
     return start;
 }
 
 int main(int argc, char** argv) {
-    char BufTotal[70] = "\x1b[32m"; // TODO max basename check
-    int len_dir;
-    const char* dir = fast_basename(&len_dir);
+
+    dir = fast_basename();
     memcpy(BufTotal+5, dir, len_dir);
+
     char* escapetemp = BufTotal+5+len_dir;
-    escapetemp[0] = '\x1b'; escapetemp[1] = '['; escapetemp[2] = '0'; escapetemp[3] = 'm'; escapetemp[4] = ' ';
+    escapetemp[0] = '\x1b'; escapetemp[1] = '['; escapetemp[2] = '0';
+    escapetemp[3] = 'm'; escapetemp[4] = ' ';
 
     int index = 10 + len_dir;
 
     char* bufindex = BufTotal+index;
+
     if (argv[1] && argv[1][0] != '0') {
-        char buf[15] = {'\x1b', '[', '3', '1', 'm', ' ', ' ', ' ', argv[1][0], ' ', '\x1b', '[', '0', 'm', '\0'};
-        if (argv[1][2] == '\0') { // second
-			buf[8] = argv[1][1];
-			buf[7] = argv[1][0];
-		} else if (argv[1][3] == '\0') { // third
-			buf[8] = argv[1][2];
-            buf[7] = argv[1][1];
-            buf[6] = argv[1][0];
-		}
+
+        char buf[15] = {'\x1b', '[', '3', '1', 'm', ' ', ' ', ' ', argv[1][0],
+                        ' ', '\x1b', '[', '0', 'm', '\0'};
+
+        if (argv[1][2] == '\0') { /* two digit status into buf */
+            buf[7] = argv[1][0]; buf[8] = argv[1][1];
+        }
+        else if (argv[1][3] == '\0') { /* three digit status into buf */
+            buf[6] = argv[1][0]; buf[7] = argv[1][1]; buf[8] = argv[1][2];
+        }
+
         memcpy(bufindex, buf, 15);
 
-    } else { // const char* arrow = "   \x1b[33m> ";
-		bufindex[0] = ' '; bufindex[1] = ' '; bufindex[2] = ' '; bufindex[3] = '\x1b'; bufindex[4] = '['; bufindex[5] = '3'; bufindex[6] = '3'; bufindex[7] = 'm'; bufindex[8] = '>'; bufindex[9] = ' '; bufindex[10] = '\x1b'; bufindex[11] = '['; bufindex[12] = '0'; bufindex[13] = 'm'; bufindex[14] = '\0';
     }
-    write(STDOUT_FILENO, BufTotal, index+20);
+    else { // const char* arrow = " \x1b[33m> ";
+        bufindex[0] = ' '; bufindex[1] = ' '; bufindex[2] = ' ';
+        bufindex[3] = '\x1b'; bufindex[4] = '['; bufindex[5] = '3';
+        bufindex[6] = '3'; bufindex[7] = 'm'; bufindex[8] = '>';
 
+        bufindex[9] = ' '; bufindex[10] = '\x1b'; bufindex[11] = '[';
+        bufindex[12] = '0'; bufindex[13] = 'm'; bufindex[14] = '\0';
+    }
+
+    write(STDOUT_FILENO, BufTotal, index+20);
 }
